9e9e241e528606af080fb916af1c8042550b62ae
carsonritchey <ritcheycarson@gmail.com>
2025-04-08 00:12:39 -0500

    SDL hello world

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// Copyright 2011 The Emscripten Authors.  All rights reserved.
+// Emscripten is available under two separate licenses, the MIT license and the
+// University of Illinois/NCSA Open Source License.  Both these licenses can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+#include <SDL/SDL.h>
+
+#include <emscripten.h>
+
+int main(int argc, char** argv) {
+  printf("hello, world!\n");
+
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Surface *screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
+
+
+  if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
+  for (int i = 0; i < 256; i++) {
+    for (int j = 0; j < 256; j++) {
+      // To emulate native behavior with blitting to screen, alpha component is ignored. Test that it is so by outputting
+      // data (and testing that it does get discarded)
+      int alpha = (i+j) % 255;
+      *((Uint32*)screen->pixels + i * 256 + j) = SDL_MapRGBA(screen->format, i, j, 255-i, alpha);
+    }
+  }
+  if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
+  SDL_Flip(screen);
+
+  printf("you should see a smoothly-colored square - no sharp lines but the square borders!\n");
+  printf("and here is some text that should be HTML-friendly: amp: |&| double-quote: |\"| quote: |'| less-than, greater-than, html-like tags: |<cheez></cheez>|\nanother line.\n");
+
+  SDL_Quit();
+
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,47 @@
+# folder build stuff goes into
+BUILD_DIR := build
+
+# the html page that emscripten compiles to 
+EMCC_HTML_FILE := shell_minimal.html
+
+# emscripten output
+TARGET := $(BUILD_DIR)/index.html
+
+# c++ source code
+SOURCES := $(wildcard *.cpp)
+HEADERS := $(wildcard *.h)
+
+# emcc flags
+# -O3 => highest optimization level
+# --shell-file => the html page emscripten compiles to
+# -s => compile for webgl2 
+FLAGS := -O3 --shell-file ${EMCC_HTML_FILE} -sMAX_WEBGL_VERSION=2
+
+# ---------------------------------------------------------------
+
+# compiles c++ code
+# default target since it's first
+build:
+	mkdir -p ${BUILD_DIR}
+	emcc $(SOURCES) -o $(TARGET) ${FLAGS}
+
+# delete build folder and files
+clean:
+	rm -rf $(BUILD_DIR)
+	@echo build directory '${BUILD_DIR}' removed!
+
+# kills any currently running instances of python3 -m http.server
+server-kill:
+	@pids=$$(ps aux | grep -E -i "[p]ython(3)? -m http.server" | awk '{print $$2}'); \
+	if [ -n "$$pids" ]; then \
+		echo "killing server with pid: $$pids"; \
+		kill -9 $$pids; \
+	else \
+		echo "no server running"; \
+	fi
+
+# stops all servers and starts a new one
+server-start:
+	${MAKE} server-kill
+	@echo "starting server in backgroud ..."
+	python3 -m http.server > /dev/null 2>&1 &
